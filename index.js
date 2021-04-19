5340f8bc7400bddcee9272f3d88656c854e8d757
Olexandr Goloshchapov <olexandr.goloshchapov@oit.edu>
2021-04-18 19:00:18 -0700

    Restructuring.

---
+++ index.js
@@ -0,0 +1,101 @@
+
+// Setup basic express server
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+let numUsers = 0;
+
+io.on('connection', (socket) => {
+
+  var userName = '';
+
+  let addedUser = false;
+  console.log('client connected to the server!');
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    
+    const messageData = JSON.parse(data)
+    const messageContent = messageData.messageContent
+    const roomName = messageData.roomName
+
+    const chatData = {
+      userName : userName,
+      messageContent : messageContent,
+      roomName : roomName
+    }
+
+    socket.broadcast.emit('updateChat',JSON.stringify(chatData))
+
+    // we tell the client to execute 'new message'
+    // socket.broadcast.emit('updateChat',{
+    //   username: data.username,
+    //   message: data.messageContent,
+    //   roomName: data.roomName
+    // });
+	
+	console.log('message: %s', data);
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
